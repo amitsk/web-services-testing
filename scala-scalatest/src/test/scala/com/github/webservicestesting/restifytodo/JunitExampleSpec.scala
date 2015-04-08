68bf23e1ac47880e0294b1d9589b3aa905dfd49b
Archana Kumar <archkum@gmail.com>
2015-04-07 22:29:31 -0700

    JUnit Test Fixed

--- scala-scalatest/src/test/scala/com/github/webservicestesting/restifytodo/JunitExampleSpec.scala
+++ scala-scalatest/src/test/scala/com/github/webservicestesting/restifytodo/JunitExampleSpec.scala
@@ -1,39 +1,29 @@
 package com.github.webservicestesting.restifytodo
 
 import com.github.webservicestesting.model.QueryResult
+import com.github.webservicestesting.model.QueryResult.Query
 import com.jayway.restassured.RestAssured
 import com.jayway.restassured.RestAssured._
 import com.jayway.restassured.http.ContentType
 import com.jayway.restassured.response.{Header, Response}
-import com.tngtech.java.junit.dataprovider.{DataProvider, UseDataProvider}
+import com.tngtech.java.junit.dataprovider.{DataProviderRunner, DataProvider, UseDataProvider}
 import org.apache.commons.lang3.StringEscapeUtils
-import org.assertj.core.api.JUnitSoftAssertions
+import org.assertj.core.api.{SoftAssertions, JUnitSoftAssertions}
+import org.junit.runner.RunWith
 import org.junit.{Before, Rule, Test}
-import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.Matchers
+import org.scalatest.junit.{ShouldMatchersForJUnit, AssertionsForJUnit}
 
-class JunitExampleSpec extends AssertionsForJUnit {
-  @Rule  var softly: JUnitSoftAssertions = new JUnitSoftAssertions
+import scala.collection.mutable
+
+class JunitExampleSpec extends AssertionsForJUnit with Matchers {
 
   @Before def initBaseUrl {
     RestAssured.baseURI = "https://query.yahooapis.com"
     RestAssured.basePath = "/v1/public/yql"
   }
-
-  @DataProvider(splitBy = "\\|", trimValues = true, format = "%m: Search %p[0] for  %p[1] to find  %p[2]") def zipSearchResult: Array[String] = {
-    return Array[String]("97229  | pizza  | Pizza Caboose", "97006  | burger | Red Robin", "97223  | coffee | Morning Rush Expresso")
-  }
-
-  @Test
-  @UseDataProvider("zipSearchResult") def localSearchByZipCodeReturnsExpectedResults(zipCode: String, query: String, businessToTest: String) {
-    val response: Response = given.header(new Header("Accept-Encoding", "gzip, deflate")).log.all.queryParam("q", generateSearchQuery(zipCode, query)).queryParam("format", "json").accept(ContentType.JSON).get
-    response.then.log.all
-    softly.assertThat(response.getStatusCode).isEqualTo(200)
-    softly.assertThat(response.getHeader("Content-Type")).contains("application/json")
-    val customerNames: java.util.List[String] = response.jsonPath.getList("query.results.Result.Title")
-    assert(customerNames contains (businessToTest))
-  }
-
   @Test def localSearchByZipCodeReturnsCompleteResponse {
+    val softly: SoftAssertions = new SoftAssertions
     val zipCode: String = "97006"
     val query: String = "pizza"
     val businessToTest: String = "Bellagios Pizza"
@@ -44,13 +34,14 @@ class JunitExampleSpec extends AssertionsForJUnit {
     val queryResultQuery: QueryResult.Query = queryResult.getQuery
     softly.assertThat(queryResultQuery.getCount).isEqualTo(10)
     softly.assertThat(queryResultQuery.getLang).isEqualToIgnoringCase("en-us")
-    //val resultsWithReviews: List[QueryResult.Query#SearchResult] = queryResultQuery.getResults.getResult.stream.filter(s -> s.getRating().getTotalReviews().compareTo("0") != 0).collect(Collectors.toList)
-    //softly.assertThat(resultsWithReviews.stream.map(QueryResult.Query.SearchResult.getTitle).collect(Collectors.toList)).contains(businessToTest)
+    softly.assertAll()
+    val results: mutable.Buffer[Query.SearchResult] = scala.collection.JavaConversions.asScalaBuffer(queryResultQuery.getResults.getResult)
+    val resultsWithReviews = results.filter( s => !(s.getRating().getTotalReviews() == "0") ).map(x=> x.getTitle())
+    resultsWithReviews should contain (businessToTest)
   }
 
   protected def generateSearchQuery(zip: String, query: String): String = {
-    val templateString: String = "select * from local.search where zip=${zip} and query='${query}'"
+    val templateString: String = s"select * from local.search where zip=${zip} and query='${query}'"
     return StringEscapeUtils.escapeHtml4(templateString)
   }
-
 }
