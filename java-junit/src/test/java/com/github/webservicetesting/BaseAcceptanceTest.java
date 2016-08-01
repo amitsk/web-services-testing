a10101579f7fb72352251144a2c1f431c80cba56
amitsk <amitsk1974@gmail.com>
2016-07-31 22:01:48 -0700

    Fixed JAckson serialization issue

--- java-junit/src/test/java/com/github/webservicetesting/BaseAcceptanceTest.java
+++ java-junit/src/test/java/com/github/webservicetesting/BaseAcceptanceTest.java
@@ -1,5 +1,11 @@
 package com.github.webservicetesting;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.ObjectMapperConfig;
+import com.jayway.restassured.config.RestAssuredConfig;
+import com.jayway.restassured.mapper.factory.Jackson2ObjectMapperFactory;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.Before;
@@ -19,5 +25,13 @@ public class BaseAcceptanceTest {
             throw new IllegalArgumentException("Please specify an environment. Valid values are local,test,prod");
         }
         config = ConfigFactory.load(env);
+
+        RestAssured.config = RestAssuredConfig.config().objectMapperConfig(new ObjectMapperConfig().jackson2ObjectMapperFactory(
+                (aClass, s) -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+                    return objectMapper;
+                }
+        ));
     }
 }
